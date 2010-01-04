41980fc901e8d334f9d9075923b0d7ec266ba71c
Alexander Osadchy <alexandr.osadchy@gmail.com>
2010-01-04 16:15:16 +0000

    New methods + JavaDoc + CodeStyle

--- java/net/anotheria/db/service/BasePersistenceServiceJDBCImpl.java
+++ java/net/anotheria/db/service/BasePersistenceServiceJDBCImpl.java
@@ -1,7 +1,9 @@
 package net.anotheria.db.service;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import net.anotheria.db.config.JDBCConfig;
 import net.anotheria.db.config.JDBCConfigFactory;
@@ -9,40 +11,127 @@ import net.anotheria.db.config.JDBCConfigFactory;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.log4j.Logger;
 
+/**
+ * Base persistence service.
+ */
 public abstract class BasePersistenceServiceJDBCImpl {
-	
-	private BasicDataSource dataSource; 
+
+	/**
+	 * DataSource.
+	 */
+	private BasicDataSource dataSource;
+
+	/**
+	 * Logger.
+	 */
 	protected Logger log;
-	
-	protected BasePersistenceServiceJDBCImpl(){
-		log = Logger.getLogger(this.getClass()); 
+
+	/**
+	 * Default constructor.
+	 */
+	protected BasePersistenceServiceJDBCImpl() {
+		log = Logger.getLogger(this.getClass());
 		init();
 	}
-	
-	public void init(){
+
+	/**
+	 * Initialize service parameters.
+	 */
+	public void init() {
 		dataSource = new BasicDataSource();
 		JDBCConfig config = JDBCConfigFactory.getJDBCConfig();
-		log.info("Using config: "+config);
+		log.info("Using config: " + config);
 		dataSource.setDriverClassName(config.getDriver());
-		dataSource.setUrl("jdbc:"+config.getVendor()+"://"+config.getHost()+":"+config.getPort()+"/"+config.getDb());
+		dataSource.setUrl("jdbc:" + config.getVendor() + "://" + config.getHost() + ":" + config.getPort() + "/" + config.getDb());
 		dataSource.setUsername(config.getUsername());
 		dataSource.setPassword(config.getPassword());
-		
+
 		if (config.getMaxConnections() != Integer.MAX_VALUE)
 			dataSource.setMaxActive(config.getMaxConnections());
 	}
-	
-	protected Connection getConnection() throws SQLException{
+
+	/**
+	 * Get connection from pool.
+	 * 
+	 * @return {@link Connection}
+	 * @throws SQLException
+	 */
+	protected Connection getConnection() throws SQLException {
 		return dataSource.getConnection();
 	}
-	
-	protected void release(Connection c){
-		if (c==null)
-			return;
-		try{
-			c.close();
-		}catch(SQLException e){
-			log.error("release - couldnt close connection", e);
+
+	/**
+	 * Close {@link Connection} if opened. All {@link SQLException} on closing are ignored.
+	 * 
+	 * @param conn
+	 *            - {@link Connection} object
+	 */
+	protected void close(Connection conn) {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+			}
 		}
+	}
+
+	/**
+	 * Close {@link Statement} if opened. All {@link SQLException} on closing are ignored.
+	 * 
+	 * @param st
+	 *            - {@link Statement} object
+	 */
+	protected void close(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+			}
+		}
+	}
+
+	/**
+	 * Close {@link ResultSet} if opened. All {@link SQLException} on closing are ignored.
+	 * 
+	 * @param st
+	 *            - {@link ResultSet} object
+	 */
+	protected void close(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+			}
+		}
+	}
+
+	/**
+	 * Close {@link Connection} if opened. All {@link SQLException} on closing are ignored.
+	 * 
+	 * @param conn
+	 *            - {@link Connection} object
+	 */
+	protected void release(Connection conn) {
+		close(conn);
+	}
+
+	/**
+	 * Close {@link Statement} if opened. All {@link SQLException} on closing are ignored.
+	 * 
+	 * @param st
+	 *            - {@link Statement} object
+	 */
+	protected void release(Statement st) {
+		close(st);
+	}
+
+	/**
+	 * Close {@link ResultSet} if opened. All {@link SQLException} on closing are ignored.
+	 * 
+	 * @param st
+	 *            - {@link ResultSet} object
+	 */
+	protected void release(ResultSet rs) {
+		close(rs);
 	}
 }
