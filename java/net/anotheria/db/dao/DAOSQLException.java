6b049c9dbd2b2d0524bd136188a5411e40b9d936
Denis Metelin <denis.metelin@nazax.com>
2010-01-15 22:27:21 +0000

    Add SQL code to DAOSQLException

--- java/net/anotheria/db/dao/DAOSQLException.java
+++ java/net/anotheria/db/dao/DAOSQLException.java
@@ -2,8 +2,35 @@ package net.anotheria.db.dao;
 
 import java.sql.SQLException;
 
-public class DAOSQLException extends DAOException{
-	public DAOSQLException(SQLException e){
+public class DAOSQLException extends DAOException {
+
+	/**
+	 * Serial version ID.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * SQL vendor specific error code.
+	 */
+	private int sqlCode;
+
+	/**
+	 * SQL State.
+	 */
+	private String sqlState;
+
+	public DAOSQLException(SQLException e) {
 		super(e.getMessage());
+		sqlCode = e.getErrorCode();
+		sqlState = e.getSQLState();
 	}
+
+	public int getSqlCode() {
+		return sqlCode;
+	}
+
+	public String getSqlState() {
+		return sqlState;
+	}
+
 }
