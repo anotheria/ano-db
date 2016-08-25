595e6404f51f1ee16aefb17f59be37441b86626b
Sebastian Davids <Sebastian.Davids@ext.parshipelite.com>
2016-08-25 16:56:07 +0200

    Do use "java.lang.Class" logger

--- src/main/java/net/anotheria/db/service/BasePersistenceServiceJDBCImpl.java
+++ src/main/java/net/anotheria/db/service/BasePersistenceServiceJDBCImpl.java
@@ -36,7 +36,7 @@ public abstract class BasePersistenceServiceJDBCImpl {
 	/**
 	 * Logger.
 	 */
-	protected Logger log = LoggerFactory.getLogger(BasePersistenceServiceJDBCImpl.class.getClass());
+	protected Logger log = LoggerFactory.getLogger(BasePersistenceServiceJDBCImpl.class);
 
 	/**
 	 * PROXY factory.
